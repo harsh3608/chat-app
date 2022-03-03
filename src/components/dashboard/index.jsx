f4aead0e2e590c4a00b01f3377f48a1419eba5cd
harsh3608 <harsh3608@gmail.com>
2022-03-03 14:08:22 +0530

    update all user references if data is updated

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Alert, Button, Divider, Drawer } from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
-import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
-import AvatarUploadBtn from './AvatarUploadBtn';
+import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
+import AvatarUploadBtn from './AvatarUploadBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
 function Dashboard({ onSignOut }) {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
-
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
 
       Alert.success('Nickname has been updated', 4000);
     } catch (err) {
@@ -37,14 +41,14 @@ function Dashboard({ onSignOut }) {
           name="nickname"
           initialValue={profile.name}
           onSave={onSave}
-          label={<h6 className="mb-2"> Nickname</h6>}
+          label={<h6 className="mb-2">Nickname</h6>}
         />
         <AvatarUploadBtn />
       </Drawer.Body>
 
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
-          Sign Out
+          Sign out
         </Button>
       </Drawer.Footer>
     </>
