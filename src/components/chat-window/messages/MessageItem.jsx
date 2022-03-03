f617698873fb29c6bcac42087940a46a85e7377a
harsh3608 <harsh3608@gmail.com>
2022-03-03 20:26:20 +0530

    displayed user data with modal window

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 function MessageItem({ message }) {
   const { author, createdAt, text } = message;
@@ -14,14 +15,20 @@ function MessageItem({ message }) {
           className="ml-1"
           size="xs"
         />
-        <span className="ml-2">{author.name}</span>
+
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
         />
       </div>
+
       <div>
-        <span className="word-break-all">{text}</span>
+        <span className="word-breal-all">{text}</span>
       </div>
     </li>
   );
