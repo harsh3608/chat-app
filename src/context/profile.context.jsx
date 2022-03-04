77eeda1ebd65d74a6699be160e399112c6e1e6dc
harsh3608 <harsh3608@gmail.com>
2022-03-04 09:35:08 +0530

    displayed real-time presence

--- src/context/profile.context.jsx
+++ src/context/profile.context.jsx
@@ -43,7 +43,7 @@ export function ProfileProvider({ children }) {
         });
 
         database.ref('.info/connected').on('value', snapshot => {
-          if (snapshot.val() === false) {
+          if (!!snapshot.val() === false) {
             return;
           }
 
